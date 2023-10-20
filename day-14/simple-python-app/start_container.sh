d19dfcd3a53aabb9fff6a8897560cb0f7655a6fb
Pawan Kumar Baggi <101386982+Pawan-jsp@users.noreply.github.com>
2023-10-20 16:41:54 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull baggipawan/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 baggipawan/simple-python-flask-app
 
