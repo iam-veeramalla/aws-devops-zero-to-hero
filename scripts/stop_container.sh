c750ce40b21c954ee9afd90e855b4db15133b3d5
Yawtin <64794644+Yawtin@users.noreply.github.com>
2024-03-03 20:31:42 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-container_id=$(docker ps --format "{{.ID}}" --filter "status=running")
-
-# Check if any running container found
-if [ -n "$container_id" ]; then
-    # Delete the running container
-    docker rm -f "$container_id"
-    echo "Deleted container with ID: $container_id"
-fi
