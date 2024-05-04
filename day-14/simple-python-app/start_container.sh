e357bf422e801091437a3b2dda27029710d8a4e5
Anusha Ananth Kirigere <52482155+Anushakirgeri@users.noreply.github.com>
2024-05-03 22:48:02 -0700

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker images from DockerHub
-docker push anushakirigere/simple-python-flask-app:tagname
+docker pull anushakirigere/simple-python-flask-app:latest
 
 # Run these Docker images as a container
-docker run -d -p 5000:5000 anushakirigere/simple-python-flask-app:tagname
+docker run -d -p 5000:5000 anushakirigere/simple-python-flask-app:latest
