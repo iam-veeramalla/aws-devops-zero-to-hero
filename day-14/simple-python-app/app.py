0fa54cfea5de488daead20061731fe3ebce05a85
Anusha Ananth Kirigere <52482155+Anushakirgeri@users.noreply.github.com>
2024-05-04 11:19:47 -0700

    msg2

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Successfull deployment of Python App on Flask using AWS CodePipeline!'
+    return 'Successfull deployment of Python App on Flask using AWS CodePipeline!!'
 
 if __name__ == '__main__':
     app.run()
