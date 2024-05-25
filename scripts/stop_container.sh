8c27f559c8de72d9c96fc243ae5e6aef5c58bef0
shivajiec <shivajieckamble@gmail.com>
2024-05-25 20:58:59 +0530

    new code

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "container has been stopped"
+
+# Stop all running containers
+echo "Stopping all running containers..."
+docker stop $(docker ps -q)
+
+# Remove all containers
+echo "Removing all containers..."
+docker rm $(docker ps -a -q)
+
+echo "All containers stopped and removed."
