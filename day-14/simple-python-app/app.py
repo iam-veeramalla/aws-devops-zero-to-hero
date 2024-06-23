fc0feebacb39e77c580fbefdc24e5a78263d033b
srihari-2908 <158026866+srihari-2908@users.noreply.github.com>
2024-06-23 11:39:59 +0530

    Update app.py
    Updates app.py to change the host

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, world!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
