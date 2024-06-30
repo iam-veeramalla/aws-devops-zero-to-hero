38db2a19f49457046773c2687167b3e177094a25
krishamhari <166092845+krishamhari@users.noreply.github.com>
2024-06-30 19:38:28 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull hariknag/simple-python-flask-app
+docker pull hariknag/sample-python-flask-app100
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 hariknag/simple-python-flask-app
+docker run -d -p 5000:5000 hariknag/sample-python-flask-app100
