6c7cc336b83016ead567d060725a1cc03e66265f
devopsenggr <152875568+devopsenggr@users.noreply.github.com>
2024-07-05 23:21:50 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-#containerid=`docker ps | awk -F " " '{print $1}'`
-#docker rm -f $containerid
+containerid=`docker ps | awk -F " " '{print $1}'`
+docker rm -f $containerid
