2d9072266db09d7bc0124ab162c4a54d3c6776c3
Ashutoshr02 <101607307+Ashutoshr02@users.noreply.github.com>
2024-07-20 23:06:42 +0530

    Update userdata1.sh

--- day-24/userdata1.sh
+++ day-24/userdata1.sh
@@ -32,7 +32,7 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <p>Welcome to CloudChamp's Channel</p>
+  <p>Welcome to Server B</p>
   
 </body>
 </html>
