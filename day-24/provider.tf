6cabe6319c8a419bbce1a663940d0eef3a0124de
Ashutoshr02 <101607307+Ashutoshr02@users.noreply.github.com>
2024-07-23 09:44:38 +0530

    Update provider.tf
    Added remote backend and table lock

--- day-24/provider.tf
+++ day-24/provider.tf
@@ -11,3 +11,12 @@ provider "aws" {
   # Configuration options
   region = "ap-south-1"
 }
+terraform {
+  backend "s3" {
+    bucket         = "mystalefile"      # Replace with your S3 bucket name
+    key            = "mystalefile/terraform.tfstate"  # The path within the bucket where the state file will be stored
+    region         = "ap-south-1"               # The region where your S3 bucket is located
+    dynamodb_table = "terraformlock"          # Optional: For state locking using DynamoDB
+    encrypt        = true                      # Encrypt the state file at rest
+  }
+}
