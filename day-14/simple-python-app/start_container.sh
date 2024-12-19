cb61d3fa11a8625ee26718d6e8d50781c7adbad6
VenkataViswas <123818703+VenkataViswas@users.noreply.github.com>
2024-12-19 20:27:56 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull abhishekf5/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
