8aa51203e71745c48d3db189ee19258063d93dba
LakshmiTV-1 <139742546+LakshmiTV-1@users.noreply.github.com>
2025-02-16 16:33:27 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull lakshmi569/ultimate-ci-cd-demo:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 lakshmi569/ultimate-ci-cd-demo:latest
+docker run -d -p 5000:5000 lakshmi569/ultimate-ci-cd-demo:latest 
